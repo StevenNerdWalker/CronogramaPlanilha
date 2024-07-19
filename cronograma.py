e82e5b18568920786fd6a924305dfcba3d3fb196
Lucas Amaral <lucasamvencio@gmail.com>
2024-07-19 00:18:44 -0300

    Updated year variable to check current year. Renamed and updated get_stage_info to get_stage_dates to also include the actual execution dates along with the planned ones. Increased search range for mondays and fridays due to possible errors. Uninverted the friday list.

--- cronograma.py
+++ cronograma.py
@@ -4,39 +4,58 @@ import datetime as time
 """OBSERVAÇÕES DE USO
 Precisa atualizar o ano para conferir as segundas-feiras
 """
-year = 2024
+year = time.datetime.now().year
 
 # givens:
-# planned_col, actual_col, stages_col, top_line (contains the dates), file_path (from C:/), first_colored_col, sheet_name
+# planned_col, actual_col, top_line (contains the dates), file_path (from C:/), first_colored_col, sheet_name
 
 file_path = 'C:/Users/USUARIO/Downloads/Cronograma.xlsx'
 sheet_name = 'Planilha1'
 
 
-def get_stage_info(file_path, sheet_name, planned_col, top_line):
+def get_stage_dates(file_path, sheet_name, planned_col, actual_col, top_line):
+    '''considera duas cells adjacentes com texto no formato "dd/mm até dd/mm" e assume que está no ano atual
+    OBS: the actual begin and finish dates won't necessarily have a numerical value, they can be None. Just the planned ones are guaranteed to have a date.'''
     wb = xls.load_workbook(filename=file_path)
     ws = wb[sheet_name]
 
     line = top_line+1
     empty_lines_counter = 0
     stages_info = []
+
     while True:
         cell1 = ws.cell(row=line, column=planned_col)
         text1 = cell1.value
+
+        cell2 = ws.cell(row=line, column=actual_col)
+        text2 = cell2.value
+
         if text1 == None:
             empty_lines_counter += 1
             line += 1
-        else:
-            substrings = text1.split(' ')
-            begin_list = substrings[0].split('/')
-            finish_list = substrings[2].split('/')
-            dates = [time.date(year, int(begin_list[1]), int(begin_list[0])), 
-                     time.date(year, int(finish_list[1]), int(finish_list[0]))]
-            info = [line]+dates
+
+        elif text1 != None:
+            substrings_planned = text1.split(' ')       #['dd/mm', 'até', 'dd/mm']  planned dates
+            begin_list_planned = substrings_planned[0].split('/')       #['dd', 'mm']
+            finish_list_planned = substrings_planned[2].split('/')      #['dd', 'mm']
+            dates_planned = [time.date(year, int(begin_list_planned[1]), int(begin_list_planned[0])), 
+                        time.date(year, int(finish_list_planned[1]), int(finish_list_planned[0]))]
+
+            if text2 != None:
+                substrings_actual = text2.split(' ')        #same but for the executed dates
+                begin_list_actual = substrings_actual[0].split('/')
+                finish_list_actual = substrings_actual[2].split('/')
+                dates_actual = [time.date(year, int(begin_list_actual[1]), int(begin_list_actual[0])), 
+                                time.date(year, int(finish_list_actual[1]), int(finish_list_actual[0]))]
+            elif text2 == None:
+                dates_actual = [None, None]
+            
+            info = [line]+dates_planned+dates_actual        #[line, planned begin date, planned finish date, actual begin date, actual finish date]
             stages_info.append(info)
 
             empty_lines_counter = 0
             line +=1
+
 
         if empty_lines_counter >= 5: 
             break
@@ -50,11 +69,11 @@ def get_weeks(begin, finish):
     day = time.timedelta(days=1)
     week = time.timedelta(days=7)
 
-    for i in range(0, 7):
+    for i in range(0, 8):
         if begin_date.isoweekday() == 1: break          # gets last monday before everything begins
         begin_date -= day
 
-    for i in range(0,7):
+    for i in range(0,8):
         if finish_date.isoweekday() == 5: break         #gets first friday after everything ends
         finish_date += day
 
@@ -70,12 +89,10 @@ def get_weeks(begin, finish):
     while friday - begin_date > time.timedelta():
         fridays.append(friday)
         friday -= week
-    fridays = fridays[::-1]
+    fridays = fridays[::-1]     #places the fridays in cronological order
     
 
-    weeks = [[mondays[i], fridays[i]] for i in range(0, len(mondays))]
-    print(len(mondays), len(fridays))
+    weeks = [[mondays[i], fridays[i]] for i in range(0, len(mondays))]      #[[monday1, friday1], [mon2, fri2], ...]
     return weeks
 
-
-print(get_weeks(time.date(2024, 7, 1), time.date(2024, 12, 31)))
+print(get_stage_dates(file_path=file_path, sheet_name=sheet_name, planned_col=6, actual_col=7, top_line=5))
